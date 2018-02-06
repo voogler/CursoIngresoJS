f0a8635199f72df60c9897eb202ae3d6d411f211
voogler <36193769+voogler@users.noreply.github.com>
2018-02-06 10:54:54 -0300

    primeros dos ejercicos

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//var nombre;
+	//esto es un comentario
+	//alert(nombre);
+	//alert("nombre");
+	//nombre="pablo";
+	//alert(nombre);
+	var nombre = prompt("ingrse nombre","");
+	alert("su nombre es "+nombre);
+
+}
+

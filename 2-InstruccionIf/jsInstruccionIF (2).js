0eb517b0e37315e42a4185a0499fc0b6e4fa7fd8
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-07 19:41:01 -0300

    primeros tres del if

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,14 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if (edad>=18) 
+	{
+		alert("eres mayor de edad");
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+edad=document.getElementById('edad').value;
+
+if (edad>=18) 
+{
+	alert("eres mayor de edad");
+}
+
+if (edad<=17) 
+{
+	alert("eres menor de edad");
+
+}
+
 
 }//FIN DE LA FUNCIÓN

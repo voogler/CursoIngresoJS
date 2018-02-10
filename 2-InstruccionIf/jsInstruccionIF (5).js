fbaf6d56ea57d2bdf22dc393deb40378dcc4f27a
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-10 17:07:20 -0300

    5 corregido

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -5,10 +5,21 @@ function Mostrar()
 var edad;
 edad= document.getElementById('edad').value;
 
+
+/*
 if (!(edad>12 && edad<18))
 
 {
 	alert("no eres adolescente");
+}
+
+*/
+
+//esta es la forma más adecuada de acerlo con un or
+if (edad<13 || edad>17)
+
+{
+	alert("No eres adolescente");
 }
 
 }//FIN DE LA FUNCIÓN

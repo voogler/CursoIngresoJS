7ee35895c3fb6c983ffd42352869a1d48b1e238b
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-10 11:38:36 -0300

    if del 1 al 5

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,15 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+edad= document.getElementById('edad').value;
+
+if (edad>12 && edad<18)
+
+{
+	alert("eres adolescente");
+}
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+edad= document.getElementById('edad').value;
+
+if (!(edad>12 && edad<18))
+
+{
+	alert("no eres adolescente");
+}
 
 }//FIN DE LA FUNCIÓN

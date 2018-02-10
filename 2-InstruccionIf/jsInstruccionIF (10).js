6ec5837759e2eaa88798e5f7cb8eb24dc6fea8de
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-10 17:42:11 -0300

    aleatorios

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,30 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var aleatorio;
+//genera entre 1 y 10, es entre 0 y 9 pero con mas uno es lo anteriorimente dicho.
+aleatorio=Math.floor(Math.random()*(9)+1);
 
+if(aleatorio==9 || aleatorio==10)
+
+	{
+		alert("EXCELENTE "+ aleatorio);	
+	}
+else
+
+	{
+
+		if (aleatorio>=4 && aleatorio<=8)
+
+		{
+			alert("APROBÓ "+aleatorio);
+		}
+
+		else
+
+		{
+			alert("Vamos, la proxima se puede "+aleatorio);
+		}	
+
+	}
 }//FIN DE LA FUNCIÓN

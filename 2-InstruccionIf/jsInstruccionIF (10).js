23af341b8c7dbd998d094b806fa363794676027b
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-14 09:59:18 -0300

    10 tabulado
    tabulado

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,35 +2,32 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-var aleatorio;
-	
-	aleatorio=Math.floor(Math.random()*(9)+1);
+	var aleatorio;
+		
+		aleatorio=Math.floor(Math.random()*(9)+1);
 
-	
-if (aleatorio==9 || aleatorio==10)
-
-
-	{
-		alert("EXCELENTE " + aleatorio)		
-	}
-
-else
-
-
-	{
-		if (aleatorio>=4 && aleatorio<=8)
-
+		
+	if (aleatorio==9 || aleatorio==10)
 
 		{
-			alert("APROBO "+aleatorio);
+			alert("EXCELENTE " + aleatorio)		
 		}
 
-		else
+	else
 
 		{
-			alert("Vamos, la proxima se puede "+aleatorio);
+			if (aleatorio>=4 && aleatorio<=8)
+
+			{
+				alert("APROBO "+aleatorio);
+			}
+
+			else
+
+			{
+				alert("Vamos, la proxima se puede "+aleatorio);
+			}
 		}
-	}
 
 
 

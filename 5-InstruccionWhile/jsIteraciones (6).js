50322c034dcbea5f73c50da06b06b842f7e58813
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-20 11:45:04 -0300

    del uno al 6 resulto

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,21 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	var promedio;
 
+	
+	while (contador<5)
+	{
+		numero=prompt("ingrese otro numero");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+		contador++;
+	}
 
+	promedio=acumulador/contador;
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN

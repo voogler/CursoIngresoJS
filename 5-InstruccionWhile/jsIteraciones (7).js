b41e19812453b7244a2e848f446d414408104e50
Ezequiel Fiasconaro <36193769+voogler@users.noreply.github.com>
2018-02-20 22:59:23 -0300

    7 y 8 resueltos

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,23 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero=0;
+	var promedio;
+	
+	
+	while (respuesta=="si")
+	{
+		
+		numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		acumulador=acumulador+ numero;
+		contador++;
+		respuesta=prompt("si para seguir");
+	}
 
+	promedio=acumulador/contador;
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,30 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
 
+	while(respuesta=="si")
+	{
+		numero=prompt("ingrese un número");
+		numero=parseInt(numero);
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		if (numero<0)
+		{
+			negativo=negativo*numero;
+		}
+
+		else
+		{
+			positivo=positivo+numero;
+		}
+
+
+		respuesta=prompt("Ingrese si para seguir cargando numero","si");
+	}
+
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
